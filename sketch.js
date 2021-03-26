0a9e056ca622033959b940a6afd33073f41e55a8
Kushagra1011 <76772359+Kushagra1011@users.noreply.github.com>
2021-03-26 15:57:46 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,24 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var box1,box2,dragonite;
+
+function setup(){
+    var canvas = createCanvas(400,400);
+    engine = Engine.create();
+    world = engine.world;
+
+    box1 = new Box(180,100,50,50);
+    box2 = new Box(200,250,50,100);
+    dragonite = new Ground(200,390,400,20);
+}
+
+function draw(){
+    background(0);
+    Engine.update(engine);
+    box1.display();
+    box2.display();
+    dragonite.display();
+}
